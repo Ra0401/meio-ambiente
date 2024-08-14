bb9e277ee8a9e2fec2b3089670d197b5c6f77903
Ra0401 <115992640+Ra0401@users.noreply.github.com>
2024-08-14 08:16:43 -0300

    script.js

--- script.js
+++ script.js
@@ -32,15 +32,15 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Qual foi a primeira grande conferência internacional, realizada pela Organização das Nações Unidas (ONU), sobre o meio ambiente em nível global??",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Conferência das Nações Unidas sobre o Meio Ambiente Humano.",
+                afirmacao: "Está correto."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Conferência das Nações Unidas para o Meio Ambiente e o Desenvolvimento.",
+                afirmacao: "Está incorreto."
             }
         ]
     },
